9e4a8bc2c8d707ecc3f5bf15a0bbcd2cf5d2b6c2
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-12 11:50:14 -0400

    Created clickable event thumbnails that take user to event clicked details

--- src/app/events/event-details/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -7,10 +8,15 @@ import { EventService } from '../../shared/event.service';
 })
 export class EventDetailsComponent implements OnInit {
   event: any;
-  
-  constructor(private eventService: EventService) {}
+
+  constructor(
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(1)
+    this.event = this.eventService.getEvent(
+      +this.activatedRoute.snapshot.params['id'] // + just casts string of id to a number, since getEvent expects id to be of type number
+    );
   }
 }
