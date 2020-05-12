2bd80add7e5ee981cd703f02d90309278cdb2844
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-12 09:59:52 -0400

    Created NavbarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
 
 @NgModule({
-  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [EventsAppComponent],
