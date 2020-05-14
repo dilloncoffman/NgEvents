1011c9da397a7d096d31ed4e9991d8a6c545c9bb
Dillon Coffman <dcoffman@ceiamerica.com>
2020-05-14 15:57:23 -0400

    Used @Input setter to create a derived value from an @Input property to then use in the template to render upvote heart as white or red based off if the user has voted or not

--- src/app/events/event-details/upvote/upvote.component.ts
+++ src/app/events/event-details/upvote/upvote.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 })
 export class UpvoteComponent implements OnInit {
   @Input() count: number;
-  @Input() voted: boolean;
+  @Input() set voted(val) {
+    this.iconColor = val ? 'red' : 'white';
+  }
   @Output() vote = new EventEmitter();
+  public iconColor: string;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    if (this.voted) {
+    } else {
+    }
+  }
 
   onClick(): void {
     this.vote.emit({});
